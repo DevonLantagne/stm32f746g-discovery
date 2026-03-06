fcc3ba78577e69ee4af8eff708c42b00c8734869
Devon <devonlantagne@me.com>
2026-03-06 11:42:09 -0600

    Added configurable input type for plot_real.m

--- matlab/plot_real.m
+++ matlab/plot_real.m
@@ -1,8 +1,10 @@
-function plot_real(filename)
-%PLOT_REAL Plot float32 samples from a raw binary memory dump.
+function plot_real(filename, opts)
+%PLOT_REAL Plot samples from a raw binary memory dump.
 %
-%   PLOT_REAL(FILENAME) reads float32 values from the binary file
-%   specified by FILENAME and plots them.
+%   PLOT_REAL(FILENAME, NameValuePairs) reads values from the binary file
+%   specified by FILENAME and plots them. Default assumes the data is in
+%   32-bit floating-point format (float32) and little-endian byte order.
+%   You can change the input format with the 'inputType' optional argument.
 %
 %   This function is intended for visualizing raw buffers dumped from
 %   embedded memory using a debugger (e.g., GDB).
@@ -14,14 +16,24 @@ function plot_real(filename)
 %   ------
 %   FILENAME      Character vector or string scalar specifying the path
 %                 to the binary file.
+%   NameValuePairs:
+%       'inputType'   Data type of the values in the binary file (default float32). Can also be
+%                     int8, int16, int32, int64, uint8, uint16, uint32, or uint64.
+%       'machineFmt'  Machine format of the values in the binary file (default little endian).
 %
 %   Example
 %   -------
-%       plot_real('buffer.bin');
+%       plot_real('buffer.bin'); % Reads float32 data from buffer.bin
+%       plot_real('buffer.bin', 'inputType', 'int16'); % Reads int16 data from buffer.bin
 %
 
     arguments
         filename {mustBeTextScalar}
+        opts.inputType {mustBeMember(opts.inputType, {...
+            'float32',...
+            'int8','int16','int32','int64',...
+            'uint8','uint16','uint32','uint64'})} = 'float32'
+        opts.machineFmt {mustBeMember(opts.machineFmt, {'ieee-le','ieee-be'})} = 'ieee-le'
     end
 
     % Open the .bin file
@@ -35,7 +47,7 @@ function plot_real(filename)
     cleanupObj = onCleanup(@() fclose(fid));
     
     % Read Data and convert to double
-    data = fread(fid, 'float32=>double');
+    data = fread(fid, 'precision', [opts.inputType '=>double'], 'machinefmt', opts.machineFmt);
 
     % Generate figure
     figure('visible', 'off');
